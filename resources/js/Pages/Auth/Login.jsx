549e45829571f4fc4759c52e0c9b4f3300ca469c
felippe.flip <felippe.flip@gmail.com>
2026-04-08 15:48:28 -0300

    v0.0.8

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -122,7 +122,7 @@ export default function Login({ status, canResetPassword }) {
                                     </div>
                                 </div>
 
-                                <Link
+                                <a
                                     href={route('auth.google')}
                                     className="w-full flex items-center justify-center gap-3 h-12 border-2 border-[#27272a] bg-[#18181b] hover:bg-[#27272a] text-xs font-bold uppercase tracking-widest text-white transition-all group"
                                 >
@@ -133,7 +133,7 @@ export default function Login({ status, canResetPassword }) {
                                         <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
                                     </svg>
                                     Google Auth Terminal
-                                </Link>
+                                </a>
                             </div>
                         </form>
                     </div>
